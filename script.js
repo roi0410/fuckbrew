fd19c7b48303cae210f2a9ef8781a6f25c1ac67d
misodengaku <misodengaku@gmail.com>
2016-03-11 00:20:21 +0900

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+var blacklist = ['brew install', 'brew update']
+
+$.each(blacklist,function(index,val){
+	var text = $("div.itemsShowBody").text();
+	if (text.indexOf(val) != -1)
+	{
+		var ret = confirm("以下の文字列が本文中に見つかりました: " + val + "\nページの表示を中止しますか？");
+		if (ret)
+		{
+			history.back();
+		}
+	}
+});
